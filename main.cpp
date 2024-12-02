678c87261d668f3e7ccb4bfd3459a275725d9e27
CharlotteEstia <c.lassalle@net.estia.fr>
2024-12-02 15:23:32 +0000

    interrupt + timer : count time pushed

--- main.cpp
+++ main.cpp
@@ -4,14 +4,48 @@
  */
 #include "mbed.h"
 
+DigitalOut led1(LED1);
+InterruptIn button(BUTTON1);
+
+using namespace std::chrono;
+Timer t;
+
 namespace {
 #define PERIOD_MS 2000ms
 }
 
+bool A = 0;
+uint64_t elapsed_time = 0; 
+
+void on_button_press()
+{
+    led1 = 1;       
+    t.start(); 
+    A=0;          
+}
+
+void on_button_release()
+{
+    led1 = 0;       
+    t.stop();   
+    elapsed_time = duration_cast<milliseconds>(t.elapsed_time()).count();    
+    t.reset();   
+    A=1;        
+}
+
 int main()
 {
+    button.rise(&on_button_press); 
+    button.fall(&on_button_release);
+
     while (true) {
-        printf("Alive! \n");
-        ThisThread::sleep_for(PERIOD_MS / 2);
+
+         if (A == 1) {
+            printf("The time taken was %llu milliseconds\n", elapsed_time);
+           
+            A = 0; 
+        }
+        
+        ThisThread::sleep_for(50ms); 
     }
 }
